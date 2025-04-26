2b05d785eb04806da22a3ef01353c78173d77621
gondrong445 <120247078+gondrong445@users.noreply.github.com>
2025-04-26 22:31:29 +0700

    Create tailwind.config.js

---
+++ frontend/tailwind.config.js
@@ -0,0 +1,16 @@
+module.exports = {
+  content: ['./src/**/*.{html,js,svelte,ts}'],
+  theme: {
+    extend: {
+      colors: {
+        'neon-blue': '#4F46E5',
+        'neon-purple': '#A855F7',
+        'dark-bg': '#1A1A1A',
+      },
+      fontFamily: {
+        poppins: ['Poppins', 'sans-serif'],
+      },
+    },
+  },
+  plugins: [],
+};
